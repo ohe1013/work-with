62672f4dad57a4fa90e916008ecabf6f168dc32a
ohe1013 <dhgusrms23@naver.com>
2023-06-14 21:39:45 +0900

    skim off vworld & ol

--- src/hooks/map/useSearch.ts
+++ src/hooks/map/useSearch.ts
@@ -1,8 +1,5 @@
-import { fetchVworldSearchResult } from "../../api/map/vworld";
-
 const searchVworld = async (params: any) => {
-    const result = await fetchVworldSearchResult(params);
-    return result;
+    return null;
 };
 
 export { searchVworld };
