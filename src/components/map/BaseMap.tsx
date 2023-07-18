db4469683dfbc59789c008ed53e4ab230bea167a
ohe1013 <84114149+ohe1013@users.noreply.github.com>
2023-07-18 09:11:12 +0900

    search

--- src/components/map/BaseMap.tsx
+++ src/components/map/BaseMap.tsx
@@ -1,62 +1,44 @@
 import { useEffect, useState } from "react";
-import { Map, MapMarker, useInjectKakaoMapApi } from "react-kakao-maps-sdk";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 import { useRecoilState } from "recoil";
 import { MapAtom, MapMarkersAtom } from "../../recoil/MapStatus";
 
 export default function BaseMap() {
-    const [markers, setMarkers] = useRecoilState(MapMarkersAtom);
-    const [map, setMap] = useRecoilState(MapAtom);
-
+    // const [markers, setMarkers] = useRecoilState(MapMarkersAtom);
     const [info, setInfo] = useState();
-    const [state, setState] = useState({
-        // 지도의 초기 위치
-        center: { lat: 33.452613, lng: 126.570888 },
-        // 지도 위치 변경시 panto를 이용할지에 대해서 정의
-        isPanto: false,
+    const [markers, setMarkers] = useRecoilState(MapMarkersAtom);
+    const [map, setMap] = useState();
+    const [zoomLevel, setZoomLevel] = useState(3);
+    const [mapInfo, setMapInfo] = useState({
+        center: {
+            lat: 37.566826,
+            lng: 126.9786567,
+        },
     });
-    const mapOption = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3, // 지도의 확대 레벨
-    };
+
     useEffect(() => {
-        console.log(map);
-        if (!map.a) return;
-        // const ps = new kakao.maps.services.Places();
-        // console.log(ps);
-        // ps.keywordSearch("인덕원", (data, status, _pagination) => {
-        //   if (status === kakao.maps.services.Status.OK) {
-        //     // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-        //     // LatLngBounds 객체에 좌표를 추가합니다
-        //     const bounds = new kakao.maps.LatLngBounds();
-        //     let markers: Marker[] = [];
+        let lat = mapInfo.center.lat;
+        let lng = mapInfo.center.lng;
+        if (markers.length === 0) return;
+        markers.map((marker) => {
+            lat += +marker.position.lat;
+            lng += +marker.position.lng;
+        });
+        console.log(lat, lng);
+        lat = lat / (markers.length + 1);
+        lng = lng / (markers.length + 1);
+        setMapInfo({ center: { lat, lng } });
+        setZoomLevel(10);
+    }, [mapInfo.center.lat, mapInfo.center.lng, markers]);
 
-        //     for (let i = 0; i < data.length; i++) {
-        //       markers.push({
-        //         position: {
-        //           lat: data[i].y,
-        //           lng: data[i].x,
-        //         },
-        //         content: data[i].place_name,
-        //       });
-        //       bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-        //     }
-        //     setMarkers(markers);
-
-        //     // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-        //   }
-        // });
-    }, [map]);
     return (
         <Map // 로드뷰를 표시할 Container
-            center={{
-                lat: 37.566826,
-                lng: 126.9786567,
-            }}
+            center={mapInfo.center}
             style={{
                 width: "100%",
                 height: "350px",
             }}
-            level={3}
+            level={zoomLevel}
             onCreate={setMap}
         >
             {markers.map((marker) => (
