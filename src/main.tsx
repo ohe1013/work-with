f3d24aebd53264f9d8dfdcf15c9aadd06e96b050
ohe1013 <84114149+ohe1013@users.noreply.github.com>
2023-07-19 10:01:24 +0900

    config change

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
 
@@ -13,9 +12,9 @@ import { RecoilRoot } from "recoil";
 
 const router = createBrowserRouter(routes);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <RouterProvider router={router} />
-    </RecoilRoot>
-  </React.StrictMode>
+    <React.StrictMode>
+        <RecoilRoot>
+            <RouterProvider router={router} />
+        </RecoilRoot>
+    </React.StrictMode>
 );
