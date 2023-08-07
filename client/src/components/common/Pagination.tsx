13f9c99bd470aeb12491f90b5f69488bb2410b4e
ohg <dhgusrms23@naver.com>
2023-08-07 22:51:53 +0900

    commit

--- client/src/components/common/Pagination.tsx
+++ client/src/components/common/Pagination.tsx
@@ -1,4 +1,14 @@
-export default function Pagination() {
+import Button from "./Button";
+
+export default function Pagination({
+  isPrev,
+  isNext,
+  pageClickHandler,
+}: {
+  isPrev: boolean;
+  isNext: boolean;
+  pageClickHandler: (type: "prev" | "next") => void;
+}) {
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
       <div className="flex flex-1 justify-between sm:hidden">
@@ -21,12 +31,15 @@ export default function Pagination() {
             className="isolate inline-flex -space-x-px rounded-md shadow-sm"
             aria-label="Pagination"
           >
-            <a
-              href="#"
-              className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
-              <span className="sr-only">{"<-"}</span>
-            </a>
+            {isPrev && (
+              <Button
+                isDisabled={!isPrev}
+                className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                onClick={() => pageClickHandler("prev")}
+              >
+                ←
+              </Button>
+            )}
             {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
             <a
               href="#"
@@ -41,12 +54,15 @@ export default function Pagination() {
             >
               10
             </a>
-            <a
-              href="#"
-              className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
-              <span className="sr-only">{"->"}</span>
-            </a>
+            {isNext && (
+              <Button
+                isDisabled={!isNext}
+                className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                onClick={() => pageClickHandler("next")}
+              >
+                →
+              </Button>
+            )}
           </nav>
         </div>
       </div>
