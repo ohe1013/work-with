d7580b7cdd8331fca58a8b5245cfe022af75ad47
ohhk <ohk@uitgis.com>
2023-08-16 17:26:05 +0900

    repair

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,13 @@
 import express from "express";
 import cors from "cors";
-import { verifyToken } from "./middleware/tokenMiddleware.js";
+import { verifyToken } from "./middleware/tokenMiddleware.ts";
 const app = express();
 app.use(cors());
-
 app.get("/", verifyToken, async (req, res) => {
-  console.log(xs);
-  res.send(xs);
+  res.send("xs ");
 });
 app.post("/api/login", verifyToken, async (req, res) => {
   res.send({ token: "hi" });
 });
 
-app.listen(3000, () => console.log("is open"));
+app.listen(3000, () => console.log("nodemon"));
--- server/src/middleware/tokenMiddleware.ts
+++ server/src/middleware/tokenMiddleware.ts
@@ -1,17 +1,14 @@
-import { getToken } from "../controller/authController";
+import { getToken } from "../controller/authController.ts";
 import jwt from "jsonwebtoken";
 
 const accessTokenSecret = "your-access-token-secret"; // Access Token의 시크릿 키
 
-const verifyToken = (req, res, next) => {
-  console.log(req);
-
+const verifyToken = (req: any, res: any, next: any) => {
   const token = getToken(req); // 토큰을 얻는 방식을 getToken 함수로 추상화
-
+  console.log("ssssssssssssstoken", token);
   if (!token) {
     return res.status(401).json({ message: "Access token not found" });
   }
-
   try {
     const decoded = jwt.verify(token, accessTokenSecret);
     req.user = decoded;
