558304b0dc750fbd2aec792ae73d9ade272503ff
ohg <dhgusrms23@naver.com>
2023-11-03 00:12:04 +0900

    test

--- client/src/api/map/kakaoSearch/index.ts
+++ client/src/api/map/kakaoSearch/index.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 export async function fetchKakaoSuggest(keyword: string) {
   const res = await axios.get(
-    "/api/v1/kakao/search/?service=local-suggest&q=" + keyword
+    "https://map.kakao.com/api/dapi/suggest/hub?service=local-suggest&q=" +
+      keyword
   );
   return res;
 }
